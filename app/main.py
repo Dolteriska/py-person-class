92eb4ffe9d6b50b2cd6a7ee4f9f5f4ca1fd62e79
Illia Savinov <dresar2404@gmail.com>
2026-02-01 16:44:24 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int, **kwargs) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    new_list = [Person(d["name"], d["age"]) for d in people]
+    for dictionary in people:
+        if dictionary.get("wife"):
+            person = Person.people[dictionary["name"]]
+            person.wife = Person.people[dictionary["wife"]]
+        if dictionary.get("husband"):
+            person = Person.people[dictionary["name"]]
+            person.husband = Person.people[dictionary["husband"]]
+    return new_list
