fae2d7b77861c98417a536710948bbcbd9f0b564
Illia Savinov <dresar2404@gmail.com>
2026-02-01 16:53:02 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@ class Person:
     def __init__(self, name: str, age: int, **kwargs) -> None:
         self.name = name
         self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     new_list = [Person(d["name"], d["age"]) for d in people]
     for dictionary in people:
         if dictionary.get("wife"):
